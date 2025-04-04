38b3a274a3809024d0ab51622672b95b5432e673
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2025-04-04 08:40:24 +0530

    making a class to handle test for password validate

---
+++ src/test/java/com/bridgelabz/PasswordValidatorTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PasswordValidatorTest {
+    @Test
+
+    void isValidTrue(){
+        Assertions.assertTrue(PasswordValidator.validPassword("@Abcdfe652"));
+    }
+    @Test
+    void isValidfalse(){
+        Assertions.assertFalse(PasswordValidator.validPassword("qAbcdfe652"));
+    }
+}
